5890043ff30c630bbf1eb88e7cc5e5ff3876e5b4
kangsanguk <63863758+kangsanguk@users.noreply.github.com>
2020-11-06 00:23:08 +0900

    Add C practice
    It is a code that outputs multiplication tables.

---
+++ hw42.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+	for (int i = 1; i <= 9; i++)
+	{
+		for (int j = 2; j <= 5; j++)
+		{
+			printf("%d * %d =%2d\t", j, i, j*i);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	for (int i = 1; i <= 9; i++)
+	{
+		for (int j = 6; j <= 9; j++)
+		{
+			printf("%d * %d =%2d\t", j, i, j*i);
+		}
+		printf("\n");
+	}
+}
